43deb74c045e646bf4f973b15796f9187ebb3875
L1nk3rrr <l1nk3rino@gmail.com>
2023-12-30 12:28:54 +0200

    lesson6

---
+++ lesson6_flask_js/app/routes.py
@@ -0,0 +1,49 @@
+import logging
+
+from flask import Blueprint, render_template, request, redirect
+
+from app.db import add_question, get_questions
+
+bp = Blueprint("tests", __name__)
+
+filename = "data.txt"
+
+
+@bp.route("/")
+def root():
+    poll_data = get_questions()
+    poll_data = [dict(row) for row in poll_data]
+    return render_template("poll.html", data=poll_data, number_of_ques=len(poll_data))
+
+
+@bp.route("/add_test", methods=["GET", "POST"])
+def add_test():
+    if request.method == "GET":
+        return render_template("add_test.html")
+    else:
+        number_of_ques = int(request.form.get("numberOfQuestions"))
+        for number in range(1, number_of_ques+1):
+            question = request.form.get(f"question{number}")
+            answers = request.form.get(f"answers{number}")
+            correct = request.form.get(f"correct{number}")
+            add_question(question, answers, correct)
+        return redirect("/")
+
+
+@bp.route("/poll")
+def poll():
+    # Потрібно перевірити чи користувач ввів правильну відповідь
+    # В базі проводимо пошук по запитанню (написати свою в файлику db.py)
+    # і перевіряєм чи співпадає з value
+    # Виводити користувачу на яку кількість запитань він правильно відповів (наприклад 10/12)
+    number_of_ques = request.args.get('numberOfQues', "NOTHING")
+    for key, value in request.args.items():
+        if "field" in key:
+            ...
+            # question = key[?:]
+            # question = use regex
+            # question = replace?
+
+    # переробити thankyou.html під це все діло
+    # return render_template("thankyou.html", data=poll_data, votes=vote)
+    return ""
